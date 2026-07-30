837a86fff6f348f8e94083150aeb6312725eb2a6
Axiomstudio <engineering@axiomstudio.ai>
2026-07-30 10:40:00 +0530

    slack: encode adapter response bodies portably

--- skills/slack/main.go
+++ skills/slack/main.go
@@ -22,7 +22,7 @@ const (
 	slackBaseURL            = "https://slack.com/api"
 	slackHTTPPort           = "50054"
 	slackSkillID            = "skill-slack"
-	slackSkillVersion       = "2.2.1"
+	slackSkillVersion       = "2.2.2"
 	slackBotTokenCredential = "slack_bot_token"
 )
 
